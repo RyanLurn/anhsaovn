85fb64faa1e41f0ffc40f3145a7571bee08c22ac
Ryan Luong <lumintriet@gmail.com>
2026-02-22 09:02:15 +0700

    feat(db): auth tables schema.

---
+++ src/db/schema/tables/auth.ts
@@ -0,0 +1,83 @@
+/* eslint-disable perfectionist/sort-objects */
+
+import {
+  timestamp,
+  boolean,
+  pgTable,
+  index,
+  text,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+import { timestamps } from "@/db/schema/helpers/timestamps";
+import { id } from "@/db/schema/helpers/id";
+
+export const userTable = pgTable("users", {
+  id,
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified").notNull(),
+  image: text("image"),
+  ...timestamps,
+});
+
+export const userId = uuid("user_id")
+  .notNull()
+  .references(() => userTable.id, { onDelete: "cascade" });
+
+export const sessionTable = pgTable(
+  "sessions",
+  {
+    id,
+    userId,
+    token: text("token").notNull().unique(),
+    expiresAt: timestamp("expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    ipAddress: text("ip_address"),
+    userAgent: text("user_agent"),
+    ...timestamps,
+  },
+  (table) => [index("sessions_user_id_idx").on(table.userId)]
+);
+
+export const accountTable = pgTable(
+  "accounts",
+  {
+    id,
+    userId,
+    accountId: text("account_id").notNull(),
+    providerId: text("provider_id").notNull(),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    accessTokenExpiresAt: timestamp("access_token_expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }),
+    refreshTokenExpiresAt: timestamp("refresh_token_expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }),
+    scope: text("scope"),
+    idToken: text("id_token"),
+    password: text("password"),
+    ...timestamps,
+  },
+  (table) => [index("accounts_user_id_idx").on(table.userId)]
+);
+
+export const verificationTable = pgTable(
+  "verifications",
+  {
+    id,
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    ...timestamps,
+  },
+  (table) => [index("verifications_identifier_idx").on(table.identifier)]
+);
